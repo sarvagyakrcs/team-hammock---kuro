dd42f5349f74b432b5f63d42421fa47d1c85b4fc
Sarvagya Kumar <sarvagyakrcs@gmail.com>
2025-08-26 10:22:06 +0530

    Refactor code structure for improved readability and maintainability

--- modules/auth/components/sign-in-component.tsx
+++ modules/auth/components/sign-in-component.tsx
@@ -6,13 +6,25 @@ import { useState } from 'react'
 import { IconUser } from "@tabler/icons-react";
 import { cn } from "@/lib/utils";
 import FormSignIn from "../forms/login/form";
+import { Session } from "next-auth";
+import UserButton from "./user-button";
 
 type Props = {
   type: "form" | "modal";
   modalLabel ?: "text" | "icon" | "both"
+  authenticated ?: "user-button" | "disappear"
+  session ?: Session | null
 };
 
-const SignInForm = ({ type, modalLabel }: Props) => {
+const SignInForm = ({ type, modalLabel, session, authenticated }: Props) => {
+  if ((authenticated === "disappear" || authenticated === undefined) && session?.user?.id) return null
+
+  if(authenticated === "user-button" && session?.user?.id) {
+    return (
+      <UserButton session={session} />
+    )
+  }
+
   const [isOpen, setIsOpen] = useState(false)
 
   if (type === "form") {
