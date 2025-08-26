600537dbd31786689b38fb182a71021cae123f33
Sarvagya Kumar <sarvagyakrcs@gmail.com>
2025-08-26 09:31:34 +0530

    feat(auth): remove deprecated authentication files and update .gitignore for env files

--- lib/auth/client.ts
+++
@@ -1,7 +0,0 @@
-import { getEnv } from "@/utils/env/get-env"
-import { createAuthClient } from "better-auth/react"
-export const authClient = createAuthClient({
-    baseURL: getEnv("BETTER_AUTH_URL"),
-})
-
-export const { signIn, signUp, useSession } = createAuthClient()
--- utils/env/get-env.ts
+++
@@ -1,7 +0,0 @@
-export const getEnv = (name: string) => {
-    const env = process.env[name];
-    if (!env) {
-        throw new Error(`Environment variable ${name} is not defined`);
-    }
-    return env;
-}
